77b373d2074dc6cc066048816b9d829e69e1a86d
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-06 00:50:25 +0100

    Complete day 1

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,34 @@ pub fn solve1_1() -> i32 {
             required.insert(n);
         }
     }
+    // unreachable
+    // TODO: consider using `Result<i32, Err>`
     return -1_i32;
 }
 
+pub fn solve1_2() -> i32 {
+    let input_string = read_input(1_i32);
+    let input_lines = input_string.split("\n");
+    // forgive me lord for what I am about to do
+    let numbers: Vec<i32> = input_lines.map(|x| x.parse::<i32>().unwrap()).collect();
+    let n = numbers.len();
+    for i in 0..n {
+        let n1 = numbers[i];
+        for j in i+1..n {
+            let n2 = numbers[j];
+            for k in j+1..n {
+                let n3 = numbers[k];
+                if (n1 + n2 + n3) == 2020_i32 {
+                    return n1 * n2 * n3;
+                }
+            }
+        }
+    }
+
+    // unreachable for nice input
+    return -1;
+}
+
 fn main() {
-    println!("{}", solve1_1());
+    println!("{}", solve1_2());
 }
